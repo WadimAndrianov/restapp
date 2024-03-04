962147e79a7e51b7905c711cbd9e6e2dd33f32a4
Вадим Андрианов <slaveofGod23@outlook.com>
2024-03-04 12:56:27 +0300

    Убрал коменты

--- src/main/java/ru/and/restapp/service/impl/StudentServiceImpl.java
+++ src/main/java/ru/and/restapp/service/impl/StudentServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.and.restapp.service.impl;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.and.restapp.model.Student;
@@ -32,27 +31,6 @@ public class StudentServiceImpl implements StudentService {
     public void deleteStudent(String studentId) {
         studentsRepository.deleteById(studentId);
     }
-
-    /*@Override
-    public Student getStudent(String studentId) {
-        try {
-            return studentsRepository.findById(studentId).get();
-        }catch(NoSuchElementException e){
-            return null;
-        }
-    }*/
-    /*
-    @Override
-    public ResponseEntity<?> getStudent(String studentId){
-        Optional<Student> optionalCont = studentsRepository.findById(studentId);
-        if(optionalCont.isEmpty())
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Студент с Id " + studentId + " Отсуствует в базе данных");
-        else {
-            Student student = optionalCont.get();
-            return ResponseEntity.ok(student);
-        }
-    }
-    */
 
     @Override
     public List<Student> getAllStudent() {
